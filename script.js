1dbdb063a2b11927139367f0a35eaeaa0f904236
Jared de Uriarte <jared.deuri@gmail.com>
2023-03-01 22:34:02 -0500

    more pics added

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-console.log('hello world')
-
-
 // lightbox element
 const lightbox = document.createElement('div')
 
@@ -16,22 +13,23 @@ const images = document.querySelectorAll('img')
 //looping through all images, adding event listener to all images
 images.forEach(image => {
 
+    //adding eventListener to each image to activate lightbox active style
     image.addEventListener('click', e => {
         lightbox.classList.add('active')
 
         // creating new image to append to lightbox element
         const img = document.createElement('img')
+
         //making source of lightbox img the same source as selected images
         img.src = image.src
 
         //prevent multiple images appearing in lightbox - looping thru all lightbox images and removing them so that.....
-        while(lightbox.firstChild){
+        while (lightbox.firstChild) {
             lightbox.removeChild(lightbox.firstChild)
         }
         //appending lightbox img to lightbox element
         lightbox.appendChild(img)
     })
-
 })
 
 //logic for clicking out of lightbox
@@ -40,9 +38,6 @@ lightbox.addEventListener('click', e => {
     //what we click on (e.target) is not equal to the element with eventListener (e.currentTarget) - basically when you click on the img the light box DOESNT disappear
     if (e.target !== e.currentTarget) return
 
-
-
     // remove active class when lightbox element is clicked
     lightbox.classList.remove('active')
-
 }) 
